a32c46110f13aa2351b80e4f685bb6d8ee4de798
Akshay Gadekar <akshay.gadekar2@bajajfinserv.in>
2021-05-31 22:34:58 +0530

    initial commit

---
+++ springbootaks/src/main/java/com/springbootazure/springbootaks/SpringbootaksApplication.java
@@ -0,0 +1,13 @@
+package com.springbootazure.springbootaks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootaksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootaksApplication.class, args);
+	}
+
+}
---
+++ springbootaks/src/test/java/com/springbootazure/springbootaks/SpringbootaksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootazure.springbootaks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootaksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
